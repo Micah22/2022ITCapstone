dbcfd81f04df865b0361fa306af7c977446ae8f2
Micah22 <micahbonner98@gmail.com>
2022-04-01 00:27:33 -0500

    more tweaks

---
+++ models/teacher.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// Define the schema
+const teacherSchema = mongoose.Schema({
+  teacherId: [Number],
+  name: String,
+  username: String,
+  password: String,
+  classes: [{ courseCode: String, courseName: String}],
+});
+
+// Register each model with Mongoose.
+// There's no need to export here because mongoose is a singleton
+mongoose.model("Teacher", teacherSchema);
