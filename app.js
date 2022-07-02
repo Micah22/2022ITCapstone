ecb0daf1b480283fcee291c2324f7b965005a0dd
Micah22 <92904865+Micah22@users.noreply.github.com>
2022-07-02 11:16:03 -0500

    php version chat

--- app.js
+++ app.js
@@ -2,33 +2,6 @@ const express = require('express');
 let app = express();
 const session = require("express-session");
 
-const io = require('socket.io')(3001)
-const users = {}
-
-io.on('connection', socket => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
-  })
-})
-
-
-
-
-
-
-
-
-
-
-
 
 // set up handlebars view engine
 let handlebars = require('express-handlebars')
