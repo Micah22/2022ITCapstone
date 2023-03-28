f003de82deb2c892e7f7c30182bdabade1b0a326
Micah22 <92904865+Micah22@users.noreply.github.com>
2023-03-27 19:46:29 -0500

    changing database over to firebase from mongo

--- credentials.js
+++ credentials.js
@@ -1,12 +1,12 @@
 module.exports = {
     mongo: {
         development: {
-            // connectionString: 'mongodb://127.0.0.1:27017/CollegeConnect?retryWrites=true&w=majority'
-            connectionString: 'mongodb+srv://test:test@cluster0.njygz.mongodb.net/?retryWrites=true&w=majority'
+            connectionString: 'mongodb://127.0.0.1:27017/CollegeConnect?retryWrites=true&w=majority'
+            // connectionString: 'mongodb+srv://test:test@cluster0.njygz.mongodb.net/?retryWrites=true&w=majority'
         },
         production: {
-            // connectionString: 'mongodb://127.0.0.1:27017/CollegeConnect?retryWrites=true&w=majority'
-            connectionString: 'mongodb+srv://test:test@cluster0.njygz.mongodb.net/?retryWrites=true&w=majority'
+            connectionString: 'mongodb://127.0.0.1:27017/CollegeConnect?retryWrites=true&w=majority'
+            // connectionString: 'mongodb+srv://test:test@cluster0.njygz.mongodb.net/?retryWrites=true&w=majority'
         }
     },
     session: {
